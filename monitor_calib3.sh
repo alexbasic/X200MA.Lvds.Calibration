77d381d4f1d2e9966891f37e63a4000c9afeeb75
Aleksandr Vladimirovich <alex_basic@mail.com>
2021-01-06 17:14:57 +0500

    added alternative config

--- monitor_calib3.sh
+++ monitor_calib3.sh
@@ -1,4 +1,4 @@
 xcalib -c -alter
-xcalib -d :0 -s 0 -red 1.05 2 95 -alter
+xcalib -d :0 -s 0 -red 1.05 2 93 -alter
 xcalib -d :0 -s 0 -green 1.02 0 100 -alter
 xcalib -d :0 -s 0 -blue 1.05 0 82 -alter
